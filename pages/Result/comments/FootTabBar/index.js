b303edc72cb3c7b0b3df253bd216aaa5a6e5bcc0
小海 刘 <xhliudh@isoftstone.com>
2022-04-10 10:35:46 +0800

    feat:result tabbarUI完成

--- pages/Result/comments/FootTabBar/index.js
+++ pages/Result/comments/FootTabBar/index.js
@@ -1,68 +1,120 @@
-const app = getApp();
-Page({
+Component({
+  options: {
+    addGlobalClass: true
+  },
+  properties: {
+    // 这里定义了innerText属性，属性值可以在组件使用时指定
+    innerText: {
+      type: String,
+      value: 'default value',
+    }
+  },
   data: {
     radio: '1',
     sortMenu:true,
     programme:true,
+    list: ['a', 'b', 'c'],
+    result: ['a'],
     tabBarData:[
       {
         text: '排序',
         isChecked: true,
+        id:1,
         active: '/assets/img/result/tabbar/active/one.png',
         noactive: '/assets/img/result/tabbar/noactive/one.png'
       },
       {
         text: '航线方案',
         isChecked: false,
+        id:2,
         active: '/assets/img/result/tabbar/active/two.png',
         noactive: '/assets/img/result/tabbar/noactive/two.png'
       },
       {
         text: '仅限最早到达',
         isChecked: false,
+        id:3,
         active: '/assets/img/result/tabbar/active/three.png',
         noactive: '/assets/img/result/tabbar/noactive/three.png'
       },
       {
         text: '仅看直达',
         isChecked: false,
+        id:4,
         active: '/assets/img/result/tabbar/active/four.png',
         noactive: '/assets/img/result/tabbar/noactive/four.png'
       },
     ]
   },
-  onChange(event) {
-    this.setData({
-      radio: event.detail,
-    })
+  methods:{
+    // radioChange(event) {
+    //   console.log('radio触发',event.detail);
+    //   this.setData({
+    //     radio: event.detail,
+    //   })
+    // },
+    changeCheckBox(event) {
+      this.setData({
+        result: event.detail,
+      });
+      console.log(event.detail,'123');
+      this.triggerEvent('tabbarchange', {result: event.detail})
+    },
+  
+    changeCheckboxtoggle(event) {
+      const { index } = event.currentTarget.dataset;
+      const checkbox = this.selectComponent(`.checkboxes-${index}`);
+      checkbox.toggle();
+    },
+    
+    onClickRadio(event) {
+      const { name } = event.currentTarget.dataset;
+      this.setData({
+        radio: name,
+      });
+      this.triggerEvent('tabbarchange', {radio:name})
+    },
+    handleClickTab (e){
+      // 传递的参数
+      let activeIndex = e.currentTarget.dataset['index'];
+      let activeData = this.data.tabBarData[activeIndex]
+      // console.log(activeData,'123');
+      let data = this.data.tabBarData.map((item,index) => {
+        index === activeIndex ? (item.isChecked = true) : (item.isChecked = false)
+        return item
+      })
+      if (activeIndex === 0){
+        this.setData({
+          sortMenu : !this.data.sortMenu,
+          programme : true,
+        })
+      } else if (activeIndex === 1){
+        this.setData({
+          programme : !this.data.programme,
+          sortMenu : true,
+        })
+      } else if(activeIndex === 2 || activeIndex === 3) {
+        this.setData({
+          programme : true,
+          sortMenu : true
+        })
+        // tabnar为 后两项时
+        this.triggerEvent('tabbarchange', {actived: activeData.id})
+      }
+      console.log(!this.data.sortMenu,'sortMenu');
+      this.setData({
+        tabBarData : data,
+      })
+
+      // console.log(this.data.tabBarData,'123123');
+    },
   },
 
   onLoad: function (options) {
        
   },
 
-  handleClickTab (e){
-    // 传递的参数
-    let activeIndex = e.currentTarget.dataset['index'];
-    let data = this.data.tabBarData.map((item,index) => {
-      index === activeIndex ? (item.isChecked = true) : (item.isChecked = false)
-      return item
-    })
-    if (activeIndex === 0 ){
-      this.setData({
-        sortMenu : !this.data.sortMenu
-      })
-    }
-    if(activeIndex === 1){
-      this.setData({
-        programme : !this.data.programme
-      })
-    }
-    
-    this.setData({
-      tabBarData : data
-    })
-  },
+
   
   /**
    * 生命周期函数--监听页面初次渲染完成
