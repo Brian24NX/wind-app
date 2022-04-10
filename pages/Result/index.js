30c2815bc82a3cea8037cd4073dcf056e7f9177d
jayce.zhang <tzzhangjun@qq.com>
2022-04-10 14:56:36 +0800

    # 搜索结果页面

--- pages/Result/index.js
+++ pages/Result/index.js
@@ -5,18 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-    viewactived:false,
-    routinglist:[],
+    isPhoneX: getApp().globalData.isPhoneX,
+    viewactived: false,
+    routinglist: [],
+    planList: [],
+    placeOfLoading: '',
+    placeOfDischarge: ''
   },
 
   onTabbarChange(event) {
-    console.log(event.detail,'传递过来tab最后2项发送筛选请求');
+    console.log(event.detail, '传递过来tab最后2项发送筛选请求');
   },
-  onChangeRadio(event){
-    console.log(event.detail,'传递过来第一项的单选框');
+  onChangeRadio(event) {
+    console.log(event.detail, '传递过来第一项的单选框');
   },
-  onChangeCheckBox(event){
-    console.log(event.detail,'传递过来第二项的多选框');
+  onChangeCheckBox(event) {
+    console.log(event.detail, '传递过来第二项的多选框');
   },
 
   /**
@@ -26,61 +30,46 @@ Page({
     wx.setNavigationBarTitle({
       title: '搜索结果',
     })
-    let resultlist=wx.getStorageSync("resultlist");
+    this.dealData()
+  },
+
+  dealData() {
+    let resultlist = wx.getStorageSync("resultlist");
     this.setData({
-      routinglist:resultlist.routings
+      routinglist: resultlist.routings,
+      placeOfLoading: resultlist.placeOfLoading,
+      placeOfDischarge: resultlist.placeOfDischarge,
     })
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+    if (!resultlist.anl && !resultlist.anl && !resultlist.cnc) {
+      this.setData({
+        planList: [],
+        viewactived: false
+      })
+    } else {
+      this.setData({
+        viewactived: true,
+        planList: [{
+          title: 'CNC',
+          value: resultlist.cnc
+        }, {
+          title: 'ANL',
+          value: resultlist.cnc
+        }, {
+          title: 'APL',
+          value: resultlist.cnc
+        }]
+      })
+    }
   },
 
   // 去详情
   toDetail(e) {
     console.log(this.data.resultlist);
-    let index= e.currentTarget.dataset.id;
-    let resultlist=wx.getStorageSync("resultlist")
+    let index = e.currentTarget.dataset.id;
+    let resultlist = wx.getStorageSync("resultlist")
     console.log(resultlist)
-    let details= resultlist.routings[index];
+    let details = resultlist.routings[index];
     wx.setStorageSync('details', details);
     wx.navigateTo({
       url: '/pages/ResultDetail/index',
