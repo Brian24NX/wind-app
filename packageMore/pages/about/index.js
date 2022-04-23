f43af059da9008f770819f35345332198ded50c6
jayce.zhang <tzzhangjun@qq.com>
2022-04-23 14:10:38 +0800

    # 关于达飞

--- packageMore/pages/about/index.js
+++ packageMore/pages/about/index.js
@@ -17,52 +17,15 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  copy() {
+    wx.setClipboardData({
+      data: 'https://www.cma-cgm.com/about/the-group',
+      success() {
+        wx.showToast({
+          title: '已复制，请前往手机浏览器打开',
+          icon: 'none'
+        })
+      }
+    })
   }
 })
