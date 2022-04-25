9ba44f51fe49732969fdd62fa26740207dc9f971
jayce.zhang <tzzhangjun@qq.com>
2022-04-25 16:04:10 +0800

    # 联系我们详情

--- packageMore/pages/contact/result/index.js
+++ packageMore/pages/contact/result/index.js
@@ -1,11 +1,13 @@
 // packageMore/pages/contact/result/index.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    navTop: app.globalData.navTop,
+    navHeight: app.globalData.navObj
   },
 
   /**
@@ -15,52 +17,24 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  // 返回
+  back() {
+    wx.navigateBack()
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  // 联系我们
+  callme(e) {
+    const phone = e.currentTarget.dataset.phone
+    wx.makePhoneCall({
+      phoneNumber: phone,
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  // 复制邮箱
+  copyEmail(e) {
+    const email = e.currentTarget.dataset.email
+    wx.setClipboardData({
+      data: email,
+    })
   }
 })
