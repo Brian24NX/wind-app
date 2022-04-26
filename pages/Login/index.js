ad2061a8e35a52397127ccb69e209745ba6b27ae
jayce.zhang <tzzhangjun@qq.com>
2022-04-26 17:45:32 +0800

    # 登录

---
+++ pages/Login/index.js
@@ -0,0 +1,26 @@
+// pages/Login/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  getMessage(e) {
+    console.log(e)
+    const data = e.detail.data[0]
+    wx.showToast({
+      title: data.access_token,
+      icon: 'none'
+    })
+  }
+})
