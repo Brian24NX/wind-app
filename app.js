6d4184c5296abeb1246402eba53bccb1f3070de9
jayce.zhang <tzzhangjun@qq.com>
2022-07-15 13:51:09 +0800

    # openId的获取

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ if (wx.getStorageSync('openId')) {
       wx.request({
         url: config[config.dev_env].url + '/api/miniapp/wx/user/' + config[config.dev_env].appId + '/login?code=' + res.code,
         success(data) {
-          wx.setStorageSync('openId', data.data.openid)
-          wx.setStorageSync('wmUserInfo', JSON.stringify({userId: data.data.openid, userTag: config.app_name, projectVersion: config.version, env: config.dev_env}))
+          wx.setStorageSync('openId', data.data.data)
+          wx.setStorageSync('wmUserInfo', JSON.stringify({userId: data.data.data, userTag: config.app_name, projectVersion: config.version, env: config.dev_env}))
         }
       })
     }
