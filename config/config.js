74362ac211a968b0b53a6b73c48afc0148d27c6c
ISOFTSTONE\myhuce <myhuce@isoftstone.com>
2023-06-20 18:20:02 +0800

    uat

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ module.exports = {
     app_name: 'CMA CGM',
     version: "v7.0.0",
     expiresIn: 30,
-    dev_env: 'pro', // 是否开发环境
-    mockLogin: false, // true=模拟登录，false=非模拟登录
+    dev_env: 'dev', // 是否开发环境
+    mockLogin: true, // true=模拟登录，false=非模拟登录
     pro: {
         url: 'https://wind.cma-cgm.com',
         fileUrl: 'https://wind-admin.cma-cgm.com/api/admin/',
